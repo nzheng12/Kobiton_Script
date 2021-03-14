a96755e9460b0590d537bbfbcb55ffbe2ba8fd37
nzheng12 <55733024+nzheng12@users.noreply.github.com>
2021-03-14 17:05:44 -0400

    Add files via upload

---
+++ Scripting challenge1.ps1
@@ -0,0 +1,18 @@
+﻿$content = Get-Content -Path .\MobileLabs.AirStream.Web1.log
+$text = $content | Select-string -Pattern "41611 2018-04-26 20:00:08.8370"
+    if($text -match "41611 2018-04-26 20:00:08.8370"){
+        Write-Output "This string exist"
+ }
+    Else{
+    Write-Output "This string does not exist"
+    }
+   
+
+$content = Get-Content -Path .\MobileLabs.AirStream.Web2.log
+$text = $content | Select-string -Pattern "41611 2018-04-26 20:00:08.8370"
+    if($text -match "41611 2018-04-26 20:00:08.8370"){
+        Write-Output "This string exist"
+ }
+    Else{
+    Write-Output "This string does not exist"
+    }
